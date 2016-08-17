3658deab2d142d2c748a70097d2176caa9186a41
Sourav Nayak <souravpn1985@gmail.com>
2016-08-17 01:19:13 -0700

    Animator for Walking

--- Assets/Zard.cs
+++ Assets/Zard.cs
@@ -5,10 +5,12 @@ public class Zard : MonoBehaviour {
 
 	private float maxLeftEdge;
 	private SpriteRenderer thisSprite;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		thisSprite = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -16,9 +18,11 @@ public class Zard : MonoBehaviour {
 
 		if(Input.GetKey(KeyCode.RightArrow)){
 			thisSprite.flipX = true;
+			animator.SetBool("isWalking", true);
 			transform.Translate(Vector3.right * Time.deltaTime * 2);
 		} else if(Input.GetKey(KeyCode.LeftArrow)){
 			thisSprite.flipX = false;
+			animator.SetBool("isWalking", true);
 			transform.Translate(Vector3.left * Time.deltaTime * 2);
 		} else if(Input.GetKey(KeyCode.UpArrow)){
 			//transform.Translate(Vector3.left * Time.deltaTime * 2);
