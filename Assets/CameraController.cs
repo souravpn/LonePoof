d15c4f1f3274f62a52938659d953606aa886072d
Sourav Nayak <souravpn1985@gmail.com>
2016-08-17 01:15:11 -0700

    initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject zard;
+
+	private Vector3 offset;
+	private float camPositionX;
+	private float camPositionZ;
+	private float camPositionY;
+
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3(0f,8f, -10f);
+		camPositionZ = -10f;
+		camPositionY = 0f;
+		//transform.position = new Vector3(10f,10f,10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camPositionX = zard.transform.position.x;
+		offset = new Vector3(camPositionX,camPositionY,camPositionZ);
+		transform.position = offset;
+	}
+
+
+}
---
+++ Assets/Zard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zard : MonoBehaviour {
+
+	private float maxLeftEdge;
+	private SpriteRenderer thisSprite;
+
+	// Use this for initialization
+	void Start () {
+		thisSprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			thisSprite.flipX = true;
+			transform.Translate(Vector3.right * Time.deltaTime * 2);
+		} else if(Input.GetKey(KeyCode.LeftArrow)){
+			thisSprite.flipX = false;
+			transform.Translate(Vector3.left * Time.deltaTime * 2);
+		} else if(Input.GetKey(KeyCode.UpArrow)){
+			//transform.Translate(Vector3.left * Time.deltaTime * 2);
+		}
+
+	}
+
+}
